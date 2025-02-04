b3c7de796f0e8b4f46563988b23aebfd9674cc5b
woolian <ianwohl@gmail.com>
2025-02-03 19:28:00 -0600

    added framerate limit with clock object and tick method

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 
+
     # game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        screen.fill((0,0,0)) # black - RGB value
         pygame.display.flip()
+        dt = clock.tick(60)/1000 # limits framerate to 60 FPS
 
 if __name__ == "__main__":
     main()
